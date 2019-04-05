8ae32995d7f2d3a86b048b5558cb88396aa4fb61
mathematicalmichael <mpilosov@gmail.com>
2019-04-05 09:13:27 -0600

    dockerspawner bug fix.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -96,7 +96,7 @@ c.DockerSpawner.extra_host_config = { 'network_mode': network_name }
 # user `jovyan`, and set the notebook directory to `/home/jovyan/work`.
 # We follow the same convention.
 notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
-c.DockerSpawner.notebook_dir = notebook_dir
+#c.DockerSpawner.notebook_dir = notebook_dir
 # Mount the real user's Docker volume on the host to the notebook user's
 # notebook directory in the container
 c.DockerSpawner.volumes = { 'hub-user-{username}': notebook_dir }
