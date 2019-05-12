1eb6d4177cbaa5eb013638fa965c8d12922e2eff
Jon Ludlam <jjl25@eumache.cl.cam.ac.uk>
2019-05-12 20:34:37 +0100

    Current working version
    Signed-off-by: Jon Ludlam <jjl25@eumache.cl.cam.ac.uk>

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,8 +1,24 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+import os
+from dockerspawner import DockerSpawner
+
+
+class VolumeCreatingSpawner(DockerSpawner):
+    """
+    DockerSpawner, but creates volumes
+    """
+
+    def start(self):
+        directory = "/users/" + self.user.name
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            os.chown(directory,1000,1000)
+        return super().start()
 
 # Configuration file for JupyterHub
-import os
+
+c.JupyterHub.log_level = 'DEBUG'
 
 c = get_config()
 
@@ -11,7 +27,7 @@ c = get_config()
 # configuration parameter.
 
 # Spawn single-user servers as Docker containers
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.JupyterHub.spawner_class = VolumeCreatingSpawner
 # Spawn containers from this image
 c.DockerSpawner.container_image = os.environ['DOCKER_NOTEBOOK_IMAGE']
 # JupyterHub requires a single-user instance of the Notebook server, so we
@@ -25,17 +41,18 @@ c.DockerSpawner.extra_create_kwargs.update({ 'command': spawn_cmd })
 network_name = os.environ['DOCKER_NETWORK_NAME']
 c.DockerSpawner.use_internal_ip = True
 c.DockerSpawner.network_name = network_name
+
 # Pass the network name as argument to spawned containers
 c.DockerSpawner.extra_host_config = { 'network_mode': network_name }
 # Explicitly set notebook directory because we'll be mounting a host volume to
 # it.  Most jupyter/docker-stacks *-notebook images run the Notebook server as
 # user `jovyan`, and set the notebook directory to `/home/jovyan/work`.
 # We follow the same convention.
-notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/opam/work'
 c.DockerSpawner.notebook_dir = notebook_dir
 # Mount the real user's Docker volume on the host to the notebook user's
 # notebook directory in the container
-c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+c.DockerSpawner.volumes = { '/users/{username}': notebook_dir }
 # volume_driver is no longer a keyword argument to create_container()
 # c.DockerSpawner.extra_create_kwargs.update({ 'volume_driver': 'local' })
 # Remove containers once they are stopped
@@ -69,18 +86,18 @@ c.JupyterHub.db_url = 'postgresql://postgres:{password}@{host}/{db}'.format(
 )
 
 # Whitlelist users and admins
-c.Authenticator.whitelist = whitelist = set()
 c.Authenticator.admin_users = admin = set()
-c.JupyterHub.admin_access = True
-pwd = os.path.dirname(__file__)
-with open(os.path.join(pwd, 'userlist')) as f:
-    for line in f:
-        if not line:
-            continue
-        parts = line.split()
-        # in case of newline at the end of userlist file
-        if len(parts) >= 1:
-            name = parts[0]
-            whitelist.add(name)
-            if len(parts) > 1 and parts[1] == 'admin':
-                admin.add(name)
+c.Authenticator.whitelist = whitelist = set()
+c.Authenticator.github_organization_whitelist = set(['ocamllabs','owlbarn','tarides','ocaml'])
+#pwd = os.path.dirname(__file__)
+#with open(os.path.join(pwd, 'userlist')) as f:
+#    for line in f:
+#        if not line:
+#            continue
+#        parts = line.split()
+#        # in case of newline at the end of userlist file
+#        if len(parts) >= 1:
+#            name = parts[0]
+#            whitelist.add(name)
+#            if len(parts) > 1 and parts[1] == 'admin':
+#                admin.add(name)
