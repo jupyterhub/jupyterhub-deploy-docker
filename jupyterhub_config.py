eb07b46ba04306af1b23700a84295d85f012517e
mathematicalmichael <mpilosov@gmail.com>
2019-06-06 12:28:43 -0600

    default upgrade now working.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -174,10 +174,10 @@ c.JupyterHub.db_url = 'postgresql://postgres:{password}@{host}/{db}'.format(
 )
 
 # Allow admin users to log into other single-user servers (e.g. for debugging, testing)?  As a courtesy, you should make sure your users know if admin_access is enabled.
-c.JupyterHub.admin_access = True 
+c.JupyterHub.admin_access = False
 
 ## Allow named single-user servers per user
-c.JupyterHub.allow_named_servers = True
+c.JupyterHub.allow_named_servers = False
 
 # Run script to automatically stop idle single-user servers as a jupyterhub service.
 c.JupyterHub.services = [
