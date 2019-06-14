a7d4511d5741fdd0a74fe865d610537142dc6adb
BobCashStory <47117399+BobCashStory@users.noreply.github.com>
2019-06-14 13:07:01 +0200

    remove tls config

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -48,9 +48,9 @@ c.JupyterHub.hub_ip = 'jupyterhub'
 c.JupyterHub.hub_port = 8080
 
 # TLS config
-c.JupyterHub.port = 443
-c.JupyterHub.ssl_key = os.environ['SSL_KEY']
-c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
+# c.JupyterHub.port = 443
+# c.JupyterHub.ssl_key = os.environ['SSL_KEY']
+# c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
 
 # Authenticate users with GitHub OAuth
 c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
