b95fbda4d86bd1b47c5afcc93e81460829660677
Jon Ludlam <jon@recoil.org>
2019-09-23 21:46:21 +0100

    Allow local use of hub
    Signed-off-by: Jon Ludlam <jon@recoil.org>

--- hub/nbgrader_config.py
+++ hub/nbgrader_config.py
@@ -1,8 +1,11 @@
 c = get_config()
 import os
 
-with open('/srv/nbgrader/FoCS/nbgrader_db.url') as f:
-    db_url = f.read()
+try:
+    with open('/srv/nbgrader/FoCS/nbgrader_db.url') as f:
+        db_url = f.read()
+except:
+    pass
 
 # This file is copied to /etc/jupyter in the docker image.
 # This file just needs to point jupyter to the right course directory. You can have one more config file in the course home directory with other configuration.
