144804833334531aef3ef9244fd2a5d3a884e973
Anton Bugreev <anton.bugreev@invitae.com>
2021-08-02 12:43:52 +0700

    AI-4213: provision environment to run static jupyterhub server

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -47,14 +47,8 @@ c.DockerSpawner.debug = True
 c.JupyterHub.hub_ip = 'jupyterhub'
 c.JupyterHub.hub_port = 8080
 
-# TLS config
-c.JupyterHub.port = 443
-c.JupyterHub.ssl_key = os.environ['SSL_KEY']
-c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
-
-# Authenticate users with GitHub OAuth
-c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
-c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
+# use http
+c.JupyterHub.port = 80
 
 # Persist hub data on volume mounted inside container
 data_dir = os.environ.get('DATA_VOLUME_CONTAINER', '/data')
