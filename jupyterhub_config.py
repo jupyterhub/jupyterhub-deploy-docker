d6a5098cdf867d74a1bbfe8ab01089bd3ee59d2d
Shane Whelan <swhelan@Shanes-MacBook-Pro.local>
2022-01-16 00:00:57 +0000

    disable github auth

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -54,8 +54,9 @@ c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
 
 # Authenticate users with GitHub OAuth
 # c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
-c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 c.JupyterHub.authenticator_class = "nativeauthenticator.NativeAuthenticator"
+c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
+
 # Persist hub data on volume mounted inside container
 data_dir = os.environ.get('DATA_VOLUME_CONTAINER', '/data')
 
