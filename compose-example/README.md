425d916901d852cda0e1f0ec9bfcb91a9c19e580
Alex Leach <zander_leach@hotmail.com>
2023-05-13 06:24:57 +0100

    Update following pre-commit lint assessment

--- compose-example/README.md
+++ compose-example/README.md
@@ -22,13 +22,13 @@ docker-compose up -d
 
 # Requirements
 
-  - `docker`
-  - [`docker-compose`](https://docs.docker.com/compose/).
-  - Optionally, a domain name for LetsEncrypt certificates
+- `docker`
+- [`docker-compose`](https://docs.docker.com/compose/).
+- Optionally, a domain name for LetsEncrypt certificates
 
 ## `Dockerfile`
 
-Defines the docker build rules for the `jupyterhub` container image.  See
+Defines the docker build rules for the `jupyterhub` container image. See
 https://jupyterhub-dockerspawner.readthedocs.io/en/latest/docker-image.html for
 details on what must be included in this image. This example builds a slimmed
 down version of jupyterhub, installing `jupyterhub_traefik_proxy` from
@@ -46,7 +46,7 @@ default with credentials of `admin` and `password`.
 
 ## `jupyterhub_config.py`
 
-jupyterhub's configuration file.  Spend some time working through this file.
+jupyterhub's configuration file. Spend some time working through this file.
 This is a minimal, but documented example that works for me. A full jupyterhub
 configuration can be obtained by running `jupyterhub --generate-config` in the
 jupyterhub container. i.e.
@@ -71,8 +71,8 @@ relevant documentation (or code) for non-default modules should be referred to.
 The static configuration file used by `traefik`. This file can be used to
 configure various features on traefik, including but not limited to:-
 
-  - [ACME certificate resolvers](https://doc.traefik.io/traefik/https/acme/)
-  - [traefik entrypoints](https://doc.traefik.io/traefik/routing/entrypoints/)
-  - [traefik log](https://doc.traefik.io/traefik/observability/logs/)
-  - [traefik API](https://doc.traefik.io/traefik/operations/api/)
+- [ACME certificate resolvers](https://doc.traefik.io/traefik/https/acme/)
+- [traefik entrypoints](https://doc.traefik.io/traefik/routing/entrypoints/)
+- [traefik log](https://doc.traefik.io/traefik/observability/logs/)
+- [traefik API](https://doc.traefik.io/traefik/operations/api/)
 
--- compose-example/jupyterhub_config.py
+++ compose-example/jupyterhub_config.py
@@ -1,55 +1,56 @@
 # Configuration file for jupyterhub.
+import os
 
-c = get_config()  #noqa
+c = get_config()  # noqa
 
-## Class for authenticating users.
+# Class for authenticating users.
 #
 # One of the benefits of using jupyterhub, is its ability to use a central
 # Identity Provider and Authentication service.
-#  
-#  Currently installed: 
+#
+#  Currently installed:
 #    - default: jupyterhub.auth.PAMAuthenticator
 #    - dummy: jupyterhub.auth.DummyAuthenticator
 #    - null: jupyterhub.auth.NullAuthenticator
 #    - pam: jupyterhub.auth.PAMAuthenticator
 #  Default: 'jupyterhub.auth.PAMAuthenticator'
-# 
+#
 # Also, check the OAuth authenticators, at:-
 #  https://oauthenticator.readthedocs.io/en/latest/tutorials/provider-specific-setup/index.html
 #
 # The 'dummy' authenticator will allow any user to login and launch a jupyter
 # notebook, so should definitely NOT BE USED in production or on publicly
 # accessible servers!
-c.JupyterHub.authenticator_class = 'dummy'
+c.JupyterHub.authenticator_class = "dummy"
 
-## The public facing URL of the whole JupyterHub application.
-#  
+# The public facing URL of the whole JupyterHub application.
+#
 #          This is the address on which the proxy will bind.
 #          Sets protocol, ip, base_url
 #  Default: 'http://:8000'
 # (dev note) This will be copied to c.Proxy.public_url
-c.JupyterHub.bind_url = 'https://hub.example.com'
+c.JupyterHub.bind_url = "https://hub.example.com"
 
-## Whether to clean up the jupyterhub-managed traefik configuration
+# Whether to clean up the jupyterhub-managed traefik configuration
 # when the Hub shuts down.
 c.JupyterHub.cleanup_proxy = True
 
-## The URL on which the Hub will listen. This is a private URL for internal
+# The URL on which the Hub will listen. This is a private URL for internal
 #  communication. Typically set in combination with hub_connect_url. If a unix
 #  socket, hub_connect_url **must** also be set.
-#  
+#
 #  For example:
-#  
+#
 #      "http://127.0.0.1:8081"
 #      "unix+http://%2Fsrv%2Fjupyterhub%2Fjupyterhub.sock"
-#  
+#
 #  .. versionadded:: 0.9
 #  Default: ''
 #
 # jupyterhub_traefik_proxy will configure the 'service' url in traefik, so this
 # needs to be accessible from traefik. By default, jupyterhub will bind to
 # 'localhost', but this will bind jupyterhub to its container name
-c.JupyterHub.hub_bind_url = 'http://hub:8000'
+c.JupyterHub.hub_bind_url = "http://hub:8000"
 
 # This sets traefik's router rule for routing traffic to the jupyterhub
 # instance.
@@ -62,16 +63,16 @@ c.JupyterHub.hub_bind_url = 'http://hub:8000'
 #
 # The default is to bind to everything, creating a path-based rule. i.e.
 #   traefik.http.routers.jupyterhub.rule=PathPrefix(`/`)
-#  
+#
 # Default: = '/'
-#   
-c.JupyterHub.hub_routespec = 'hub.example.com/'
+#
+c.JupyterHub.hub_routespec = "hub.example.com/"
 
-## Set the log level by value or name.
+# Set the log level by value or name.
 #  Choices: any of [0, 10, 20, 30, 40, 50, 'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL']
 #  Default: 30
 #  See also: Application.log_level
-c.JupyterHub.log_level = 'DEBUG'
+c.JupyterHub.log_level = "DEBUG"
 
 # Use jupyterhub_traefik_proxy's `TraefikFileProviderProxy` class
 c.JupyterHub.proxy_class = "traefik_file"
@@ -95,34 +96,33 @@ c.TraefikFileProviderProxy.traefik_api_password = "password"
 # traefik's static configuraiton file, traefik.yaml, and refer to the
 # reference documentation at:-
 #   https://doc.traefik.io/traefik/https/acme/
-#c.TraefikFileProviderProxy.traefik_cert_resolver = "leresolver"
+# c.TraefikFileProviderProxy.traefik_cert_resolver = "leresolver"
 
-## The class to use for spawning single-user servers.
-#  
-#  Currently installed: 
+# The class to use for spawning single-user servers.
+#
+#  Currently installed:
 #    - default: jupyterhub.spawner.LocalProcessSpawner
 #    - localprocess: jupyterhub.spawner.LocalProcessSpawner
 #    - simple: jupyterhub.spawner.SimpleLocalProcessSpawner
 #  Default: 'jupyterhub.spawner.LocalProcessSpawner'
 #
 # Launch each user's notebook server in a separate container.
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
 
 # Base Image to use for user notebook containers. You can build your own,
 # or use an image name and tag from hub.docker.com, or another image repository
-c.DockerSpawner.image = 'jupyterhub/singleuser'
+c.DockerSpawner.image = "jupyterhub/singleuser"
 
 # Explicitly set notebook directory because we'll be mounting a host volume to
 # it.  Most jupyter/docker-stacks *-notebook images run the Notebook server as
 # user `jovyan`, and set the notebook directory to `/home/jovyan/work`.
 # We follow the same convention.
-import os
-notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
+notebook_dir = os.environ.get("DOCKER_NOTEBOOK_DIR") or "/home/jovyan/work"
 c.DockerSpawner.notebook_dir = notebook_dir
 
 # Create per-user docker volumes, mounted to the user's notebook_dir in the
 # container
-c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+c.DockerSpawner.volumes = { "jupyterhub-user-{username}": notebook_dir }
 
 # The docker network name that single-user notebook containers should attach to
 c.DockerSpawner.network_name = "traefik_internal"
@@ -131,7 +131,7 @@ c.DockerSpawner.network_name = "traefik_internal"
 # value. (This gets around a validate rule on 'proxy.bind_url', which
 # forces redirects to 'http', unless there is a value in ssl_cert).
 # Otherwise, when logging in, there will always be 302 redirects to http://
-c.JupyterHub.ssl_cert = 'externally managed'
+c.JupyterHub.ssl_cert = "externally managed"
 
 # jupyterhub will only configure path-based routing by default. To stop
 # traefik from routing all requests to jupyterhub, a subdomain host should be
@@ -146,5 +146,4 @@ c.JupyterHub.ssl_cert = 'externally managed'
 # e.g. A user of "jbloggs", logging into a hub with a subdomain_host of
 # "https://hub.example.com", will be redirected to their notebook at
 # https://jbloggs.hub.example.com
-c.JupyterHub.subdomain_host = 'https://hub.example.com'
-
+c.JupyterHub.subdomain_host = "https://hub.example.com"
